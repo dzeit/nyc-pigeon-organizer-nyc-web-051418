1fcf8e2f492467d5254ee7057cc85a5c81761e67
David Zeit <dzeit216@gmail.com>
2018-05-07 18:29:47 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
         names.each do |pigeon_name| #additional iteration in order to access the elements in the array. Because pigeon_names will become keys in new_hash.
          
         if new_hash[pigeon_name] # Since names repeat, we don't want to reset the value. 
-        binding.pry 
+       
           new_hash[pigeon_name][key] = [] # will add the next two keys i.e. gender and lives.
         else 
           new_hash[pigeon_name] = {} # This is the syntax for creating the new key in new_hash and it's value is an empty hash.
