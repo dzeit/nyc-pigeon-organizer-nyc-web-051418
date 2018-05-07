00f045ba5bb6aa56015897b7b41859bf326152fa
David Zeit <dzeit216@gmail.com>
2018-05-07 18:39:40 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -10,7 +10,7 @@ def nyc_pigeon_organizer(data)
        binding.pry 
           if hash[inner_key].include?(pigeon_name)
           new_hash[pigeon_name][key] << inner_key.to_s
-          
+         end
         else 
           binding.pry 
           new_hash[pigeon_name] = {} # This is the syntax for creating the new key in new_hash and it's value is an empty hash.
