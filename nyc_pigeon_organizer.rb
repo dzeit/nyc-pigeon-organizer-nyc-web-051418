4076946b7a748faee60a9fa16230986a8a12bc42
David Zeit <dzeit216@gmail.com>
2018-05-07 18:48:01 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -15,6 +15,7 @@ def nyc_pigeon_organizer(data)
         else 
           new_hash[pigeon_name][key] = []
           new_hash[pigeon_name][key] << inner_key.to_s
+          binding.pry 
           end 
          end
         else 
