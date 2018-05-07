0286c3240705e5211e2f6a4b4a4dfebe7b57a15f
David Zeit <dzeit216@gmail.com>
2018-05-07 18:30:54 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -8,7 +8,7 @@ def nyc_pigeon_organizer(data)
          
         if new_hash[pigeon_name] # Since names repeat, we don't want to reset the value. 
        
-          #new_hash[pigeon_name][key] = [] # will add the next two keys i.e. gender and lives.
+          new_hash[pigeon_name][key] = [] # will add the next two keys i.e. gender and lives.
         else 
           new_hash[pigeon_name] = {} # This is the syntax for creating the new key in new_hash and it's value is an empty hash.
           new_hash[pigeon_name][key] = [] # dynamic for color, gender, lives.
