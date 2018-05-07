2a535bd9ca874863f5cb10adc5ade9918a05e4f9
David Zeit <dzeit216@gmail.com>
2018-05-07 18:29:29 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -11,7 +11,7 @@ def nyc_pigeon_organizer(data)
           new_hash[pigeon_name][key] = [] # will add the next two keys i.e. gender and lives.
         else 
           new_hash[pigeon_name] = {} # This is the syntax for creating the new key in new_hash and it's value is an empty hash.
-          new_hash[pigeon_name][key] = [] # dynamic for color, gender, lives.
+          # new_hash[pigeon_name][key] = [] # dynamic for color, gender, lives.
         end 
          
         if hash[inner_key].include?(pigeon_name)
