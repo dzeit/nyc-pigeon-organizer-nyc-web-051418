1687d5a5a325339f81719904bc02283124c37a14
David Zeit <dzeit216@gmail.com>
2018-05-07 18:50:01 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -22,7 +22,7 @@ def nyc_pigeon_organizer(data)
           #binding.pry 
           new_hash[pigeon_name] = {} # This is the syntax for creating the new key in new_hash and it's value is an empty hash.
           new_hash[pigeon_name][key] = [] # dynamic for color, gender, lives.
-          
+          new_hash[pigeon_name][key] << inner_key.to_s
         end 
          
         
