e6671dff057151e5c46b7f50527204efd86fae4c
David Zeit <dzeit216@gmail.com>
2018-05-07 18:51:25 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,28 +5,20 @@ def nyc_pigeon_organizer(data)
     data.each do |key, hash| #dynamic for color, gender, lives.
       hash.each do |inner_key, names|
         names.each do |pigeon_name| #additional iteration in order to access the elements in the array. Because pigeon_names will become keys in new_hash.
-         
         if new_hash[pigeon_name] # Since names repeat, we don't want to reset the value. 
-       #binding.pry 
           if hash[inner_key].include?(pigeon_name)
-            #binding.pry 
             if new_hash[pigeon_name][key]
           new_hash[pigeon_name][key] << inner_key.to_s
         else 
           new_hash[pigeon_name][key] = []
           new_hash[pigeon_name][key] << inner_key.to_s
-          #binding.pry 
           end 
          end
         else 
-          #binding.pry 
           new_hash[pigeon_name] = {} # This is the syntax for creating the new key in new_hash and it's value is an empty hash.
           new_hash[pigeon_name][key] = [] # dynamic for color, gender, lives.
           new_hash[pigeon_name][key] << inner_key.to_s
-        end 
-         
-        
-          #binding.pry 
+        end
           
         end 
        end 
